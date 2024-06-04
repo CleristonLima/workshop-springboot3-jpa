e87563421c9492f488bd3aa61068eed22ad8d6fb
Cleriston Lima <cleriston-santos2012@hotmail.com>
2024-06-04 19:07:24 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,10 +3,20 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id // Indica qual e a chave
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Para informar que o ID e auto increment
 	private Long id;
 	private String name;
 	private String email;
